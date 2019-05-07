ea4019ee78e255dcbd2108f2a1303a93f36a60d0
Vo Minh Luan <vominhluan88@gmail.com>
2019-05-07 09:00:41 +0700

    update COURSE_DISCOVERY_FILTERS

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -304,7 +304,7 @@ FEATURES = {
     'ENABLE_COURSE_DISCOVERY': True,
 
     # Setting for overriding default filtering facets for Course discovery
-    COURSE_DISCOVERY_FILTERS = ["org", "language", "modes"]
+    # COURSE_DISCOVERY_FILTERS = ["org", "language", "modes"]
 
     # Software secure fake page feature flag
     'ENABLE_SOFTWARE_SECURE_FAKE': False,
